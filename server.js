9fb32b5c4070a02379d64e7dee58f913d4af5e48
wilmer_sanchez <wilmerjaviers@yahoo.com>
2025-02-13 19:35:05 -0600

    listo.

--- server.js
+++ server.js
@@ -25,6 +25,71 @@ const usuarios = [
     }
 ];
 
+const categorias=[
+    {
+        "id": 1,
+        "nombre": "Historia"
+    },
+    {
+        "id": 2,
+        "nombre": "Ciencia"
+    },
+    {
+        "id": 3,
+        "nombre": "Tecnología"
+    },
+    {
+        "id": 4,
+        "nombre": "Cultura"
+    },
+    {
+        "id": 5,
+        "nombre": "Medio Ambiente"
+    }
+]
+
+const pedidos = [
+    { id: 1, usuario: "Juan", total: 500 },
+    { id: 2, usuario: "María", total: 750 },
+    { id: 3, usuario: "Carlos", total: 320 },
+    { id: 4, usuario: "Ana", total: 600 }
+];
+
+const productos = [
+    {
+        id: 1,
+        nombre: "laptop",
+        precio: "1200"
+    },
+    {
+        id: 2,
+        nombre: "cargador",
+        precio: "330"
+    },
+    {
+        id: 3,
+        nombre: "memoria",
+        precio: "430"
+    },
+    {
+        id: 4,
+        nombre: "mouse",
+        precio: "120"
+    }
+];
+
+
+app.get('/api/productos', (req, res) => {
+    res.json(productos);
+});
+
+app.get("/api/pedidos", (req, res) => {
+    res.json(pedidos);
+});
+
+app.get('/api/categorias', (req, res)=>{
+    res.json(categorias);
+})
 
 app.get('/api/usuarios', (req, res) => {
     res.json(usuarios);
